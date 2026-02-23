c9458c4db7ebe76eee0a7862a7a616eab4f31634
SowmyaBakka <87377594+SowmyaBakka@users.noreply.github.com>
2026-02-23 11:23:07 +0530

    Create playwright/tests/google-search.spec.ts

---
+++ playwright/tests/google-search.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Google search', () => {
+  test('searches for hello', async ({ page }) => {
+    // TEST_BODY_PLACEHOLDER
+  });
+});
